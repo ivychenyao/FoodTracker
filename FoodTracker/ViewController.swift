6d5d8375baa4b256dba04776e9c24494fa23573f
Ivy Chenyao <ivychenyao@Ivys-MacBook-Air.local>
2016-10-15 22:57:55 -0400

    Set Default Label Text button action added

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    // MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: AnyObject) {
+        mealNameLabel.text = "Default Text"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
