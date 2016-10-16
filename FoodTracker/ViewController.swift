a2623e4dcb5f7f3255327bd56c68f915416c5cc1
Ivy Chenyao <ivychenyao@Ivys-MacBook-Air.local>
2016-10-16 00:05:16 -0400

    Added image selection

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,24 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     // MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     // MARK: Actions
     @IBAction func setDefaultLabelText(_ sender: AnyObject) {
         mealNameLabel.text = "Default Text"
     }
     
+    @IBAction func selectImageFromPhotoLibrary(_ sender: AnyObject) {
+        nameTextField.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true) { 
+            
+        }
+    }
     
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        photoImageView.image = selectedImage
+        dismiss(animated: true, completion: nil)    
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         nameTextField.delegate = self
+        print("viewDidLoad")
         
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
     }
     
     // MARK: UITextFieldDelegate
